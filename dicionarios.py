798820e4be55447867c004c1c1d842e7081708b6
dianaRodriguess <dianarodrigues044@gmail.com>
2024-05-02 00:07:20 -0300

    add items teste (cliente teste, produto teste...)

--- dicionarios.py
+++ dicionarios.py
@@ -1,9 +1,9 @@
-clientes = {
-  
-}
-produtos = {
+'''
+clientes - nome, telefone, email, endereço
+produtos - nome, quantidade
+vendas - cliente, produto, qtdvendida, data, fdp
+'''
 
-}
-vendas = {
-  
-}
+clientes = {"1": ["Cliente Teste", "00000", "teste@teste.com", "Rua Teste"]}
+produtos = {"1": ["Produto Teste", "9999"]}
+vendas = {"1": ["Cliente Teste", "Produto Teste", "999", "00/00/0000", "PIX"]}
