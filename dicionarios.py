4f9fb1589edb707ecbd540ee85c86bf2c3ae51a1
dianaRodriguess <dianarodrigues044@gmail.com>
2024-05-08 11:37:09 -0300

    add item preco em produtos

--- dicionarios.py
+++ dicionarios.py
@@ -1,9 +1,9 @@
-'''
+"""
 clientes - nome, telefone, email, endereço
-produtos - nome, quantidade
+produtos - nome, quantidade, preço
 vendas - cliente, produto, qtdvendida, data, fdp
-'''
+"""
 
 clientes = {"1": ["Cliente Teste", "00000", "teste@teste.com", "Rua Teste"]}
-produtos = {"1": ["Produto Teste", "9999"]}
+produtos = {"1": ["Produto Teste", "9999", "0.50"]}
 vendas = {"1": ["Cliente Teste", "Produto Teste", "999", "00/00/0000", "PIX"]}
