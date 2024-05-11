dcc9bbb7fc02ec310a719a7d892d2289136e7aff
Diana Rodrigues <38289073+dianaRodriguess@users.noreply.github.com>
2024-05-11 19:51:16 +0000

    escrever os dicionarios nos arquivos

--- dicionarios.py
+++ dicionarios.py
@@ -1,4 +1,3 @@
-import json
 import pickle
 
 """
@@ -9,22 +8,9 @@ vendas - cliente, produto, qtdvendida, data, fdp
 
 clientes = {"1": ["Cliente Teste", "00000", "teste@teste.com", "Rua Teste"]}
 produtos = {"1": ["Produto Teste", "9999", "0.50"]}
-# mudar vendas id cliente e id produto
-vendas = {"1": ["Produto Teste", "Cliente Teste", "999", "00/00/0000", "PIX"]}
+vendas = {"1": ["1", "1", "999", "00/00/0000", "PIX"]}
 
-
-# with open("clientes.json", "w") as file:
-#     json.dump(clientes, file)
-
-# with open("produtos.json", "w") as file:
-#     json.dump(produtos, file)
-
-# with open("vendas.json", "w") as file:
-#     json.dump(vendas, file)
-
-
-# aqui
-
+# escrever o dicionário no arquivo
 try:
     arq_clientes = open("clientes.dat", "rb")
     clientes = pickle.load(arq_clientes)
