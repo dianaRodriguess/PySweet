68a68ec706cea0c43a5754306411b1cea667a558
Diana Rodrigues <38289073+dianaRodriguess@users.noreply.github.com>
2024-06-07 23:01:52 +0000

    add arquivo para as validações

---
+++ validacoes.py
@@ -0,0 +1,17 @@
+def validarNome():
+    nome = input("##### Nome: ")
+    if nome.isalpha():
+        nome = nome
+    else:
+        print("Opa! Nome iválido, digite novamente.")
+    print()
+def validarTelefone():
+    print()
+def validarEmail():
+    print()
+def validarData():
+    print()
+def validarHora():
+    print()
+def validarFormaPagamento():
+    print()
