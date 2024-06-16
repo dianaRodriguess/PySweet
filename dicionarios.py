d3603739972d5c89a7eb1ef13b93dd6d980d46b4
Diana Rodrigues <38289073+dianaRodriguess@users.noreply.github.com>
2024-06-16 18:34:02 +0000

    add dicio formas_pagamento

--- dicionarios.py
+++ dicionarios.py
@@ -10,6 +10,8 @@ clientes = {"1": ["Cliente Teste", "00000", "teste@teste.com", "Rua Teste"]}
 produtos = {"1": ["Produto Teste", "9999", "0.50"]}
 vendas = {"1": ["1", "1", "999", "PIX", "0.50", "01/01/01, 00:00:00"]}
 
+formas_pagamento = {1: "Cartão de Credito", 2: "Cartão de Débito", 3: "Especie", 4: "Pix"}
+
 # escrever o dicionário no arquivo
 try:
     arq_clientes = open("clientes.dat", "rb")
