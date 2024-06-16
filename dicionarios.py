866b5b35f0b234c1905c4cfebb755ec6b32b4ef1
Diana Rodrigues <38289073+dianaRodriguess@users.noreply.github.com>
2024-06-16 23:51:24 +0000

    organização dinionário vendas

--- dicionarios.py
+++ dicionarios.py
@@ -3,12 +3,12 @@ import pickle
 """
 clientes - nome, telefone, email, endereço
 produtos - nome, quantidade, preço
-vendas -  id_produto, id_cliente, qtdvendida, valor, form.pagamento, data
+vendas -  id_produto, id_cliente, qtdvendida, forma_pagamento, valor, data
 """
 
 clientes = {"1": ["Cliente Teste", "00000", "teste@teste.com", "Rua Teste"]}
 produtos = {"1": ["Produto Teste", "9999", "0.50"]}
-vendas = {"1": ["1", "1", "999", "PIX", "0.50", "01/01/01, 00:00:00"]}
+vendas = {"1": ["1", "1", "999", "1", "0.50", "01/01/01, 00:00:00"]}
 
 formas_pagamento = {1: "Cartão de Credito", 2: "Cartão de Débito", 3: "Especie", 4: "Pix"}
 
