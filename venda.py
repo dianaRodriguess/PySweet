bfa0b315e2902f9a0f4b9a6a401dbe6d8fd55e0e
dianaRodriguess <dianarodrigues044@gmail.com>
2024-07-18 21:15:26 -0300

    add atualizar_quantidade()

--- venda.py
+++ venda.py
@@ -1,9 +1,11 @@
+import produto as pdt
 import interfaces as ifc
 from datetime import datetime
 from dicionarios import clientes, produtos, formas_pagamento, vendas
 from validacoes.validacoes import validarFormaPagamento
 
-def exibirVenda(id_venda):
+
+def exibir_venda(id_venda):
     cliente = clientes[vendas[id_venda][1]][0]
     produto = produtos[vendas[id_venda][0]][0]
     qtd_vendida = vendas[id_venda][2]
@@ -21,12 +23,13 @@ def exibirVenda(id_venda):
     print(f"Data: {data}")
     print("Id de cadastro: ", id_venda)
 
-def cadastrarVenda():
-    ifc.cabecalhoModulos("Cadastrar Venda")
+
+def cadastrar_venda():
+    ifc.cabecalho_modulos("Cadastrar Venda")
     id_produto = input("##### Produto vendido: ")
     print()
     id_cliente = input("##### Cliente: ")
-    
+
     if id_produto not in produtos.keys():
         print("Produto não encotrado. ")
     elif id_cliente not in clientes.keys():
@@ -37,37 +40,55 @@ def cadastrarVenda():
         print()
         valor_total = input("##### Total: ")
         print()
-        forma_pagamento = int(input("##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: "))
+        forma_pagamento = int(
+            input(
+                "##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: "
+            )
+        )
 
         while not validarFormaPagamento(formas_pagamento, forma_pagamento):
             print("Forma de pagamento invalida. Por favor escola novamente. ")
-            forma_pagamento = input("##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: " )
+            forma_pagamento = input(
+                "##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: "
+            )
 
         data = datetime.now()
-        
+
+        qdt_atual = pdt.atualizar_quantidade(id_produto, int(qtd_vendida))
+        produtos[id_produto][1] = qdt_atual
+
         id_venda = vendas.__len__() + 1
         id_venda = str(id_venda)
-        vendas[id_venda] = [id_produto,id_cliente,qtd_vendida,forma_pagamento,valor_total,data.strftime("%x, %X"),]
-        exibirVenda(id_venda)
+        vendas[id_venda] = [
+            id_produto,
+            id_cliente,
+            qtd_vendida,
+            forma_pagamento,
+            valor_total,
+            data.strftime("%x, %X"),
+        ]
+        exibir_venda(id_venda)
         print("Venda cadastrada com sucesso!")
     print()
     input("Tecle <ENTER> para continuar... ")
 
-def pesquisarVenda():
-    ifc.cabecalhoModulos("Pesquisar Venda")
+
+def pesquisar_venda():
+    ifc.cabecalho_modulos("Pesquisar Venda")
     id_venda = input("Qual o id da venda? ")
 
     if id_venda in vendas.keys():
         print()
-        exibirVenda(id_venda)
+        exibir_venda(id_venda)
         print()
     else:
         print("Não foi possível achar a venda. Tem certeza que ela está cadastrada? ")
     print()
     input("Tecle <ENTER> para continuar... ")
 
-def atualizarVenda():
-    ifc.cabecalhoModulos("Atualizar Venda")
+
+def atualizar_venda():
+    ifc.cabecalho_modulos("Atualizar Venda")
     id_venda = input("Qual o id da venda? ")
     print()
 
@@ -81,44 +102,59 @@ def atualizarVenda():
         data = input("##### Data (data, hora): ")
         print()
         valor_total = input("##### Total: ")
-        forma_pagamento = int(input("##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: "))
+        forma_pagamento = int(
+            input(
+                "##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: "
+            )
+        )
 
         while not validarFormaPagamento(formas_pagamento, forma_pagamento):
             print("Forma de pagamento invalida. Por favor escola novamente. ")
-            forma_pagamento = input("##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: " )
+            forma_pagamento = input(
+                "##### Forma de pagamento: \n##### 1 - Cartão de Débito \n##### 2 - Cartão de Crédito \n##### 3 - Espécie \n##### 4 - PIX \n: "
+            )
 
         if id_produto in produtos.keys():
             if id_cliente in clientes.keys():
-                vendas[id_venda] = [id_produto,id_cliente, qtd_vendida,forma_pagamento,valor_total,data]
+                vendas[id_venda] = [
+                    id_produto,
+                    id_cliente,
+                    qtd_vendida,
+                    forma_pagamento,
+                    valor_total,
+                    data,
+                ]
 
-                exibirVenda(id_venda)
+                exibir_venda(id_venda)
                 print("Dados da venda atualizado com sucesso!")
             else:
                 print("Cliente não encontrado. ")
         else:
             print("Produto não encontrado. ")
     else:
-        print("Não foi possível encontrar a venda. Tem certeza que ela está cadastrada?")
+        print(
+            "Não foi possível encontrar a venda. Tem certeza que ela está cadastrada?"
+        )
     print()
     input("Tecle <ENTER> para continuar... ")
 
-def deletarVenda():
-    ifc.cabecalhoModulos("Deletar Venda")
+
+def deletar_venda():
+    ifc.cabecalho_modulos("Deletar Venda")
     id_venda = input("Qual o id da venda? ")
 
     if id_venda in vendas.keys():
         print()
-        exibirVenda(id_venda)
+        exibir_venda(id_venda)
 
         resp = input("Tem certeza que deseja excluir esta venda (S/N)? ").lower()
 
-        if resp == 's':
+        if resp == "s":
             del vendas[id_venda]
             print("Venda excluida com sucesso! ")
         else:
             print("Não foi possível excluir a venda. ")
     else:
-        print("Venda não encontrada. Tem certeza que ela está cadastrada?")      
+        print("Venda não encontrada. Tem certeza que ela está cadastrada?")
     print()
     input("Tecle <ENTER> para continuar... ")
-
