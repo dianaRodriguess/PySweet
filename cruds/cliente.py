ee42e59683f63a29cecbe86e75a59ba762c28cfa
dianaRodriguess <dianarodrigues044@gmail.com>
2024-07-20 03:26:48 -0300

    update final 1 nova forma de guardar endereço; add loops

--- cruds/cliente.py
+++ cruds/cliente.py
@@ -1,109 +1,148 @@
 import funcoes
 import interfaces as ifc
 from dicionarios import clientes
+from random import randint
 
 
 def exibir_cliente(id_cliente):
+    print()
     cliente = clientes[id_cliente][0]
     telefone = clientes[id_cliente][1]
     email = clientes[id_cliente][2]
     endereco = clientes[id_cliente][3]
+    rua = endereco['rua']
+    bairro = endereco['bairro']
+    num_casa = endereco['num_casa']
+    logradouro = f'R. {rua} {num_casa}, {bairro}'
     print()
-    print(f"Nome: {cliente}")
-    print(f"Telefone: {telefone}")
-    print(f"Email: {email}")
-    print(f"Endereço: {endereco}")
-    print("Id de cadastro: ", id_cliente)
+    print(f'Nome: {cliente}')
+    print(f'Telefone: {telefone}')
+    print(f'Email: {email}')
+    print(f'Cidade: {endereco["cidade"]}')
+    print(f'Endereço: {logradouro}')
+    print('Id de cadastro: ', id_cliente)
+    print()
+
+
+def cliente_indicio(codigo):
+    if codigo in clientes:
+        return False
+    return True
+
+
+def gerar_codigo_cliente():
+    codigo = randint(0, 999999)
+    
+    codigo_formatado = f'{codigo:05d}'
+    while not (cliente_indicio(codigo_formatado)):
+        codigo = randint(0, 999999)
+        codigo_formatado = f'{codigo:05d}'
+    return codigo_formatado
 
 
 def cadastrar_cliente():
-    ifc.cabecalho_modulos("Cadastrar Cliente")
+    ifc.cabecalho_modulos('Cadastrar Cliente')
     nome_cliente = funcoes.ler_nome()
-    print()
     telefone_cliente = funcoes.ler_telefone()
-    print()
     email_cliente = funcoes.ler_email()
-    print()
-    endereço_cliente = input("##### Endereço: ")
+    cidade_cliente = funcoes.ler_cidade()
+    bairro_cliente = funcoes.ler_logradouro('Bairro')
+    rua_cliente = funcoes.ler_logradouro('Rua')
+    num_casa_cliente = funcoes.ler_logradouro('Número da casa')
 
-    id_cliente = clientes.__len__() + 1
-    id_cliente = str(id_cliente)
+    id_cliente = gerar_codigo_cliente()
     clientes[id_cliente] = [
         nome_cliente,
         telefone_cliente,
         email_cliente,
-        endereço_cliente,
+        {
+            'cidade': cidade_cliente,
+            'bairro': bairro_cliente,
+            'rua': rua_cliente,
+            'num_casa': num_casa_cliente,
+        },
     ]
 
     exibir_cliente(id_cliente)
-    print("\nCliente cadastrado com sucesso!")
+    print('Cliente cadastrado com sucesso!')
     print()
-    input("Tecle <ENTER> para continuar... ")
+    input('Tecle <ENTER> para continuar... ')
 
 
 def pesquisar_cliente():
-    ifc.cabecalho_modulos("Pesquisar Cliente")
-    id_cliente = input("Qual o id do cliente? ")
-
-    if id_cliente in clientes.keys():
+    ifc.cabecalho_modulos('Pesquisar Cliente')
+    id_cliente = input('Qual o id do cliente? ')
+    sair = 'n'
+    while sair == 'n':    
+        if id_cliente in clientes.keys():
+            exibir_cliente(id_cliente)
+        else:
+            print('\nNão foi possível encontrar o cliente. Tem certeza que ele está cadastrado? ')
+        
+        sair = input('\nDeseja sair do módulo "Pesquisar Cliente" (S/N)? ').lower()
         print()
-        exibir_cliente(id_cliente)
-        print()
-    else:
-        print(
-            "Não foi possível encontrar o cliente. Tem certeza que ele está cadastrado? "
-        )
+        if sair == "n":
+            id_cliente = input('Qual o id do cliente? ')
     print()
-    input("Tecle <ENTER> para continuar... ")
+    input('Tecle <ENTER> para continuar... ')
 
 
 def atualizar_cliente():
-    ifc.cabecalho_modulos("Atualizar Cliente")
-    id_cliente = input("Qual o id do cliente? ")
+    ifc.cabecalho_modulos('Atualizar Cliente')
+    id_cliente = input('Qual o id do cliente? ')
+    sair = 'n'
+    while sair == 'n':
+        if id_cliente in clientes.keys():
+            nome_cliente = funcoes.ler_nome()
+            telefone_cliente = funcoes.ler_telefone()
+            email_cliente = funcoes.ler_email()
+            cidade_cliente = funcoes.ler_cidade()
+            bairro_cliente = funcoes.ler_logradouro('Nome do Bairro')
+            rua_cliente = funcoes.ler_logradouro('Nome da Rua')
+            num_casa_cliente = funcoes.ler_logradouro('Número da casa')
 
-    if id_cliente in clientes.keys():
-        print()
-        nome_cliente = funcoes.ler_nome_regex()
-        print()
-        telefone_cliente = funcoes.ler_telefone()
-        print()
-        email_cliente = funcoes.ler_email()
-        print()
-        endereço_cliente = input("##### Endereço: ")
-
-        clientes[id_cliente] = [
+            clientes[id_cliente] = [
             nome_cliente,
             telefone_cliente,
             email_cliente,
-            endereço_cliente,
+            {
+                'cidade': cidade_cliente,
+                'bairro': bairro_cliente,
+                'rua': rua_cliente,
+                'num_casa': num_casa_cliente,
+            },
         ]
-        exibir_cliente(id_cliente)
-        print("\nDados do Cliente adualizado com sucesso!")
-    else:
-        print(
-            "Não foi possível encontrar o cliente. Tem certeza que ele está cadastrado?"
-        )
+            print('Novos dados do cliente: ')
+            exibir_cliente(id_cliente)
+            print('Dados do cliente atualizado com sucesso!')
+        else:
+            print('\nNão foi possível encontrar o cliente. Tem certeza que ele está cadastrado?')
+        
+        sair = input('\nDeseja sair do módulo "Pesquisar Cliente" (S/N)? ').lower()
+        print()
+        if sair == "n":
+            id_cliente = input('Qual o id do cliente? ')
     print()
-    input("Tecle <ENTER> para continuar... ")
+    input('Tecle <ENTER> para continuar... ')
 
 
 def deletar_cliente():
-    ifc.cabecalho_modulos("Deletar Cliente")
-    id_cliente = input("Qual o id do cliente? ")
+    ifc.cabecalho_modulos('Deletar Cliente')
+    id_cliente = input('Qual o id do cliente? ')
     print()
 
     if id_cliente in clientes.keys():
         exibir_cliente(id_cliente)
 
-        resp = input("\nTem certeza que deseja deletar este cliente (S/N)? ").lower()
+        resp = input('Tem certeza que deseja remover este cliente (S/N)? ').lower()
 
-        if resp == "s":
+        if resp != 'n':
             del clientes[id_cliente]
-            print("Cliente excluido com sucesso! ")
+            print('Cliente excluido com sucesso! ')
         else:
-            print("Não foi possível excluir o cliente. ")
+            print('Não foi possível excluir o cliente. ')
     else:
-        print("Cliente não encontrado. Tem certeza que ele está cadastrado?")
+        print('Cliente não encontrado. Tem certeza que ele está cadastrado?')
 
     print()
-    input("Tecle <ENTER> para continuar... ")
+    input('Tecle <ENTER> para continuar... ')
