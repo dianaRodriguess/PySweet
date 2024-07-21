33cdbff2fff14f89c002943ce346d31ad0ea05d6
dianaRodriguess <dianarodrigues044@gmail.com>
2024-07-21 03:26:20 -0300

    add novas funções ler_cidade(), ler_logradouro(), ler_codigo(), ler_form_pag(), truncate_string(), codigo_indicio(), gerar_codigo()

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,9 @@
 import pickle
+from random import randint
 import re
 from validacoes import validacoes
 import interfaces as ifc
-from dicionarios import clientes, produtos, vendas
+from dicionarios import clientes, produtos, vendas, formas_pagamento
 
 
 # escerver o dicionario no arquivo
@@ -24,62 +25,115 @@ def escrever_arquivos():
 #####  FUNÇÕES LER  #####
 #########################
 def ler_nome():
-    nome = input("##### Nome: ")
+    print()
+    nome = input("››››› Nome: ")
     while not validacoes.validar_nome(nome):
-        print("##### Ops! O nome informado é inválido! Tente novamente...")
+        print("»› Ops! Algo deu errado! \n»› Digite apenas letras. Tente novamente...")
         print()
-        nome = input("##### Nome: ")
+        nome = input("››››› Nome: ")
     nome = nome.strip().title()
     nome = re.sub(r"\s+", " ", nome)
     return nome
 
 
 def ler_nome_regex():
-    nome = input("##### Nome: ")
+    print()
+    nome = input("››››› Nome: ")
     while not validacoes.validar_nome_regex(nome):
-        print("##### Ops! O nome informado é inválido! Tente novamente...")
+        print("»› Ops! Algo deu errado! \n»› Digite apenas letras. Tente novamente...")
         print()
-        nome = input("##### Nome: ")
+        nome = input("››››› Nome: ")
     return nome
 
 
 def ler_telefone():
-    tel = input("##### Telefone: ")
+    print()
+    tel = input("››››› Telefone (somente números): ")
     while not (validacoes.validar_telefone(tel)):
-        print("##### Ops! O celular informado é inválido! Tente novamente...")
+        print("»› Ops! Algo deu errado! \n»› Digite apenas números. Tente novamente...")
         print()
-        tel = input("##### Telefone: ")
+        tel = input("››››› Telefone (somente números): ")
     return tel
 
 
 def ler_email():
-    email = input("##### Email: ")
+    print()
+    email = input("››››› Email: ")
     while not (validacoes.validar_email(email)):
-        print("##### Ops! O Email informado é inválido! Tente novamente...")
+        print(
+            "»› Ops! Algo deu errado! \n»› Digite um email válido. Tente novamente..."
+        )
         print()
-        email = input("##### Email: ")
+        email = input("››››› Email: ")
     return email
 
 
 def ler_quantidade():
-    qtd = input("##### Quantidade: ")
+    print()
+    qtd = input("››››› Quantidade (somente números): ")
     while not validacoes.validar_quantidade(qtd):
-        print("##### Ops! Aceitamos somete números! Tente novamente...")
+        print("»› Ops! Algo deu errado! \n»› Digite apenas números. Tente novamente...")
         print()
-        qtd = input("##### Quantidade: ")
+        qtd = input("››››› Quantidade (somente números): ")
     return qtd
 
 
 def ler_preco():
-    preco = input("##### Preço: ")
+    print()
+    preco = input("››››› Preço da Unidade (00,00): ")
     while not validacoes.validar_preco(preco):
-        print("##### Ops! Aceitamos somete números! Tente novamente...")
+        print("»› Ops! Algo deu errado! \n»› Siga o padrão (00,00). Tente novamente...")
         print()
-        preco = input("##### Preço: ")
+        preco = input("››››› Preço da Unidade (00,00): ")
     return preco
 
 
-# def lerEndereço():
+def ler_cidade():
+    print()
+    cidade = input("››››› Cidade (inclua os acentos): ").upper()
+    while not validacoes.validar_cidade(cidade):
+        print(
+            "»› Ops! Essa cidade não existe no Brasil. \n»› Lembre de incluir os acentos. Tente novamente..."
+        )
+        print()
+        cidade = input("››››› Cidade (inclua os acentos): ").upper()
+    return cidade
+
+
+def ler_logradouro(parte):
+    print()
+    logra = input(f"››››› {parte}: ")
+    while not validacoes.validar_logradouro(logra):
+        print(
+            '»› Ops! Algo deu errado! \n»› Digite apenas letras, números e "-". Tente novamente...'
+        )
+        print()
+        logra = input(f"››››› {parte}: ")
+    logra = logra.strip().title()
+    logra = re.sub(r"\s+", " ", logra)
+    return logra
+
+
+def ler_codigo(texto, dicio):
+    print()
+    codigo = input(f"››››› {texto}")
+    while not validacoes.validar_codigo(dicio, codigo):
+        print("»› Ops! Algo deu errado! \n»› Tem certeza que este é código certo? Tente novamente...")
+        print()
+        codigo = input(f"››››› {texto}")
+    return codigo
+
+
+def ler_form_pag():
+    print()
+    forma_pagamento = input("››››› Forma de pagamento: \n››››› 1 - Cartão de Débito \n››››› 2 - Cartão de Crédito \n››››› 3 - Espécie \n››››› 4 - PIX \n: ")
+    
+    while not validacoes.validar_codigo(formas_pagamento, forma_pagamento):
+        print("\n»› Ops! Algo deu errado! \n»› Escolha uma forma de pagamento válida. Tente novamente...")
+        
+        forma_pagamento = input("\n››››› Forma de pagamento: \n››››› 1 - Cartão de Débito \n››››› 2 - Cartão de Crédito \n››››› 3 - Espécie \n››››› 4 - PIX \n: ")
+        
+    return forma_pagamento
 
 
 #########################
@@ -87,48 +141,77 @@ def ler_preco():
 #########################
 def menu_principal():
     ifc.interface_menu_principal()
-    op_mprinc = input("##### Escolha sua opção: ")
+    op_mprinc = input("››››› Escolha sua opção: ")
     return op_mprinc
 
 
 ##### MÓDULO CADASTRAR #####
 def menu_cadastrar():
     ifc.interface_menu_cadastrar()
-    op_mcadas = input("##### Escolha sua opção: ")
+    op_mcadas = input("››››› Escolha sua opção: ")
     return op_mcadas
 
 
 ##### MÓDULO PESQUISAR #####
 def menu_pesquisar():
     ifc.interface_menu_pesquisar()
-    op_mpesq = input("##### Escolha sua opção: ")
+    op_mpesq = input("››››› Escolha sua opção: ")
     return op_mpesq
 
 
 ##### MÓDULO ATUALIZAR #####
 def menu_atualizar():
     ifc.interface_menu_atualizar()
-    op_matua = input("##### Escolha sua opção: ")
+    op_matua = input("››››› Escolha sua opção: ")
     return op_matua
 
 
 ##### MÓDULO DELETAR #####
 def menu_deletar():
     ifc.interface_menu_deletar()
-    op_mdele = input("##### Escolha sua opção: ")
+    op_mdele = input("››››› Escolha sua opção: ")
     return op_mdele
 
 
 ##### MÓDULO RELATÓRIO #####
 def menu_relatorio():
     ifc.interface_menu_relatorio()
-    op_mrela = input("##### Escolha sua opção: ")
+    op_mrela = input("››››› Escolha sua opção: ")
     return op_mrela
 
 
-##############################
-#####     INFORMAÇÕES    #####
-##############################
+##### MÓDULO INFORMAÇÕES #####
 def informacoes():
     ifc.interface_informacoes()
-    input("Tecle <ENTER> para continuar... ")
+    input("»› Tecle <ENTER> para continuar... ")
+
+
+##############################
+#####         ÚTIL       #####
+##############################
+
+
+#####      TRUNCATE      #####
+# ChatGPT
+def truncate_string(s, length):
+    if len(s) > length:
+        return s[: length - 3] + "..."
+    return s
+
+
+##### CHECAR DE CÓDIGO #####
+def codigo_indicio(codigo, dicio):
+    if codigo in dicio:
+        return False
+    return True
+
+
+##### GERAR DE CÓDIGO #####
+def gerar_codigo(dicio):
+    codigo = randint(0, 999999)
+
+    codigo_formatado = f"{codigo:06d}"
+    while not (codigo_indicio(codigo_formatado, dicio)):
+        codigo = randint(0, 999999)
+        codigo_formatado = f"{codigo:06d}"
+    return codigo_formatado
