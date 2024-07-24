6ac9292c87885d54eccbfed00469ab78cf364b07
dianaRodriguess <dianarodrigues044@gmail.com>
2024-07-24 00:20:53 -0300

    add __init__.py e testes_pesquisa.py

---
+++ cruds/__init__.py
@@ -0,0 +1,4 @@
+from .cliente import cadastrar_cliente, pesquisar_cliente, atualizar_cliente, deletar_cliente  # Assuming you want to import these specific functions
+from .produto import cadastrar_produto, pesquisar_produto, atualizar_produto, deletar_produto
+from .venda import cadastrar_venda, pesquisar_venda, atualizar_venda
+
